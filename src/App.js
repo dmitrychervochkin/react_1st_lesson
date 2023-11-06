a198dfe9e34418ef7621e433e0451a3a5510e601
Dmitry Chervochkin <chervochkin2001@mail.ru>
2023-11-06 18:06:23 +0300

    create lesson

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { createElement } from 'react';
+
+export function App() {
+	return createElement(
+		'div',
+		{className: 'App'},
+		createElement (
+			'header',
+			{className: 'App-header'},
+			createElement (
+				'img',
+				{src: logo, className: 'App-logo', alt: "logo"}
+			),
+			createElement (
+				'p',
+				null,
+				'Edit ',
+				createElement ('code', null, 'src/App.js'),
+				' and save to reload.'
+			),
+			createElement (
+				'a',
+				{className: "App-link", href: 'https://reactjs.org',
+				target: '_blank', rel: 'noopener noreferrer'},
+				'Learn React'
+			)
+		)
+	);
+}
